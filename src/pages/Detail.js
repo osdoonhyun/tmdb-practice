87eedc1a91339a0d1dafaa646fd40551056e65a0
osdoonhyun <skw1996@naver.com>
2023-05-29 23:43:05 +0900

    tmdb 정보 변경

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,13 +2,15 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+const API_KEY = process.env.TMDB_API_KEY;
+
 export default function Detail() {
   const { detailId } = useParams(); // url에 있는 path(id값) 가져오기
   const [movie, setMovie] = useState({});
 
   const getDetailMovie = async (id) => {
     try {
-      const address = `https://api.themoviedb.org/3/movie/${id}?api_key=8597e491ed6e80f0de12e349eb60ea6e`;
+      const address = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
       const { data, status } = await axios.get(address);
       console.log('++++++', data.title);
       console.log('++status++++', status);
--- src/pages/Moives.js
+++ src/pages/Moives.js
@@ -11,8 +11,7 @@ export default function Moives() {
   const options = {
     headers: {
       accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTk3ZTQ5MWVkNmU4MGYwZGUxMmUzNDllYjYwZWE2ZSIsInN1YiI6IjViNjJhOWNmMGUwYTI2N2VlNzAyYjdkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-nEypJq66ar-tr-KtFz-AC910YhdLakTDSM-oeIDLwQ',
+      Authorization: process.env.TMDB_AUTHORIZATION,
     },
   };
 
