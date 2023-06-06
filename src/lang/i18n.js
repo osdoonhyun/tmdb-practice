3e67dc81167ae8a97b1fdb3c058d933da24a1fb2
osdoonhyun <skw1996@naver.com>
2023-06-06 21:28:20 +0900

    다국어기능 일본어 중국어 추가

--- src/lang/i18n.js
+++ src/lang/i18n.js
@@ -4,6 +4,7 @@ import { initReactI18next } from 'react-i18next';
 import langEn from './lang.en.json';
 import langKo from './lang.ko.json';
 import langJp from './lang.jp.json';
+import langCn from './lang.cn.json';
 
 const resource = {
   'en-US': {
@@ -15,6 +16,9 @@ const resource = {
   'jp-JP': {
     translation: langJp,
   },
+  'cn-CN': {
+    translation: langCn,
+  },
 };
 
 i18n.use(initReactI18next).init({
@@ -22,6 +26,7 @@ i18n.use(initReactI18next).init({
   // 초기 설정 언어
   lng: 'ko-KR',
   fallbackLng: {
+    'cn-CN': ['cn-CN'],
     'jp-JP': ['jp-JP'],
     'en-US': ['en-US'],
     default: ['ko-KR'],
