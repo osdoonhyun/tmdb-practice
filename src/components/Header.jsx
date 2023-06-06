0e10d3f098d6c25a804dff5eaeb9ed5ceea289dc
osdoonhyun <skw1996@naver.com>
2023-06-06 15:05:25 +0900

    다국어처리 설정, Header에 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,32 @@
 import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 export default function Header() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
         <Container>
-          <Navbar.Brand href='#home'>타이틀</Navbar.Brand>
+          <Navbar.Brand href='#home'>{t('title')}</Navbar.Brand>
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link href='/'>Movie</Nav.Link>
-              <Nav.Link href='/tv'>Tv</Nav.Link>
-              <NavDropdown title='Dropdown' id='collasible-nav-dropdown'>
-                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.2'>
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.3'>
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href='#action/3.4'>
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href='/'>{t('movie')}</Nav.Link>
+              <Nav.Link href='/tv'>{t('tv')}</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href='/login'>LogIn</Nav.Link>
-              <Nav.Link href='/signup'>SignUp</Nav.Link>
-              <Nav.Link href='/profile'>Profile</Nav.Link>
+              <Nav.Link href='/login'>{t('login')}</Nav.Link>
+              <Nav.Link href='/signup'>{t('signup')}</Nav.Link>
+              <Nav.Link href='/profile'>{t('profile')}</Nav.Link>
+
+              <NavDropdown title={t('langselect')} id='collasible-nav-dropdown'>
+                <NavDropdown.Item href='#action/3.2'>한국어</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>영어</NavDropdown.Item>
+                {/* <NavDropdown.Divider /> */}
+                <NavDropdown.Item href='#action/3.4'>일본어</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.4'>중국어</NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
