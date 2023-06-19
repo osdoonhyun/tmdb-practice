f166168680d71368f0e7fa7962d1d3e3a105519f
osdoonhyun <skw1996@naver.com>
2023-06-19 22:31:12 +0900

    Product reducer 적용, store 추가

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,34 +1,36 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import {
+  Alert,
+  Card,
+  Col,
+  Container,
+  ListGroup,
+  Nav,
+  Row,
+  Spinner,
+} from 'react-bootstrap';
 import '../styles/Custom.css';
 import ProductModal from '../components/ProductModal';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProductsAction } from '../actions/productActions';
 
 export default function Products() {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const { category } = useParams();
+  // const [products, setProducts] = useState([]);
   const [modalShow, setModalShow] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
   const location = useLocation();
   const selectedCategory = location.state?.category;
 
+  const getProducts = useSelector((state) => state.getProducts);
+  const { loading, error, products } = getProducts;
+
   useEffect(() => {
-    getProducts();
-  }, []);
-
-  const getProducts = async () => {
-    try {
-      const address = 'http://localhost:8080/api/products';
-
-      const { data, status } = await axios.get(address);
-      console.log('productData', data);
-      if (status === 200) {
-        setProducts(data.products);
-      }
-    } catch (error) {
-      console.log('productError', error.message);
-    }
-  };
+    dispatch(getProductsAction(category));
+  }, [category, dispatch]);
 
   const onClickCard = (product) => {
     console.log('selectedProduct', selectedProduct);
@@ -41,42 +43,45 @@ export default function Products() {
 
   return (
     <div style={{ backgroundColor: '#F5F5F7' }}>
+      {loading && <Spinner animation='border' />}
+      {error && <Alert variant='danger'>{error}</Alert>}
       <Container className='pt-5'>
         <Row>
-          {products.map((product) => (
-            <Col className='mt-4'>
-              <Card
-                onClick={() => onClickCard(product)}
-                className='custom-card'
-                key={product._id}
-              >
-                <Card.Img
-                  variant='top'
-                  src={product.image}
-                  style={{
-                    maxHeight: '420px',
-                    width: '100%',
-                    objectFit: 'cover',
-                  }}
-                />
-                <Card.Body>
-                  <Card.Title>{product.name}</Card.Title>
-                  <Card.Text>{product.brand}</Card.Text>
-                </Card.Body>
-                <ListGroup className='list-group-flush'>
-                  <ListGroup.Item>{product.category}</ListGroup.Item>
-                  <ListGroup.Item>{product.createdAt}</ListGroup.Item>
-                  <ListGroup.Item style={{ height: '100px' }}>
-                    {product.description}
-                  </ListGroup.Item>
-                </ListGroup>
-                <Card.Body>
-                  <Card.Link href='#'>Card Link</Card.Link>
-                  <Card.Link href='#'>Another Link</Card.Link>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+          {products &&
+            products.map((product) => (
+              <Col className='mt-4'>
+                <Card
+                  onClick={() => onClickCard(product)}
+                  className='custom-card'
+                  key={product._id}
+                >
+                  <Card.Img
+                    variant='top'
+                    src={product.image}
+                    style={{
+                      maxHeight: '420px',
+                      width: '100%',
+                      objectFit: 'cover',
+                    }}
+                  />
+                  <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Text>{product.brand}</Card.Text>
+                  </Card.Body>
+                  <ListGroup className='list-group-flush'>
+                    <ListGroup.Item>{product.category}</ListGroup.Item>
+                    <ListGroup.Item>{product.createdAt}</ListGroup.Item>
+                    <ListGroup.Item style={{ height: '100px' }}>
+                      {product.description}
+                    </ListGroup.Item>
+                  </ListGroup>
+                  <Card.Body>
+                    <Card.Link href='#'>Card Link</Card.Link>
+                    <Card.Link href='#'>Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
         </Row>
       </Container>
       <ProductModal
--- src/store.jsx
+++ src/store.jsx
@@ -3,10 +3,12 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { userLoginReducer, userSignupReducer } from './reducers/userReducer';
+import { productReducer } from './reducers/productReducer';
 
 const reducer = combineReducers({
   userLogin: userLoginReducer,
   userSignup: userSignupReducer,
+  getProducts: productReducer,
 });
 
 const userInfoFromStorage = localStorage.getItem('userInfo') // 또는 'token'
