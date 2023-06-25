2dbd26306e3af35fca8fe83d8f8b93a5e0c86cb2
osdoonhyun <skw1996@naver.com>
2023-06-25 23:14:21 +0900

    로그아웃 시 메인페이지로 이동

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -11,6 +11,8 @@ import {
 
 const mainAddress = 'http://localhost:8080/api';
 // 로그인 처리 actions  //dispatch 가 리덕스에서 사용되는 비동기처리 메서드
+
+// 네트워킹에 따른 상태 포인트 지정 데이터를 던져준다.
 const loginUser = (email, password) => async (dispatch) => {
   try {
     dispatch({
@@ -28,7 +30,7 @@ const loginUser = (email, password) => async (dispatch) => {
         payload: data, // 데이터가 들어감
       });
     }
-    localStorage.setItem('token', data.token); // 이렇게 하면 프로필 정보 가져올 때
+    localStorage.setItem('token', data.token); // 이렇게 하면 프로필 정보 가져올 때 토큰을 이용
     localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
     dispatch({
@@ -47,6 +49,7 @@ const logoutUser = () => (dispatch) => {
   dispatch({
     type: USER_LOGOUT,
   });
+  document.location.href = '/';
 };
 
 const signupUser = (email, password, name) => async (dispatch) => {
